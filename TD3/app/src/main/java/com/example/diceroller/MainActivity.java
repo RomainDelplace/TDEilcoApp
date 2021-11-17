2cd562d2e30f07b840b0299d292f018e7866cb5e
Romain DELPLACE <romain.delplace@gmail.com>
2021-11-17 14:47:16 +0100

    TD3 fini

--- TD3/app/src/main/java/com/example/diceroller/MainActivity.java
+++ TD3/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button rollButton = (Button) findViewById(R.id.button); // récupération id boutton
+        rollButton.setOnClickListener(new View.OnClickListener() { // Si on appuie sur le boutton
+            @Override
+            public void onClick(View view) {
+                EditText Edit = findViewById(R.id.EditText); // récupération id de l'Edit Text
+                String nombre=Edit.getText().toString(); // récuréation de la valeur entrée par l'utilisateur
+                boolean verifier = TextUtils.isDigitsOnly(nombre); // vérification si la valeur est bien un chiffre/nombre
+                if (verifier && !nombre.isEmpty()) { //Si la valeur est correct
+                    int resultat = Integer.parseInt(nombre); // On transforme la valeur récupéré en int
+                    int rand = (int) (Math.random() * (resultat - 0)) + 1; // On crée un nombre aléatoire qui renvoi un nombre entre 1 et le chiffre entré
+                    TextView Text = findViewById(R.id.textView); // On va cherche l'id du textview
+                    Text.setText(String.valueOf(rand));// On écrit a l'emplacement du text du textView la valeur aléatoire créait précédement
+                    rand = (int) (Math.random() * (resultat - 0)) + 1;
+                    TextView Text2 = findViewById(R.id.textView3); // On va cherche l'id du textview
+                    Text2.setText(String.valueOf(rand));
+                }
+                else{ // Si la valeur ne correspond pas on fait un toast pour prévenir l'utilisateur
+                    Toast toast = Toast.makeText(MainActivity.this, "Valeur incorrect ou inexistant", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
 }
