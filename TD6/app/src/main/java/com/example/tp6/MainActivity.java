9846f45bae8f55048715ccdc7119c3d1699c341f
Romain DELPLACE <romain.delplace@gmail.com>
2021-12-01 08:44:56 +0100

    Ajout Repo

--- TD6/app/src/main/java/com/example/tp6/MainActivity.java
+++ TD6/app/src/main/java/com/example/tp6/MainActivity.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GithubService githubService = new Retrofit.Builder()
+                .baseUrl(GithubService.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService.class);
+
+        githubService.listRepos("adrienbusin").enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                afficherRepos(response.body());
+            }
+
+            @Override
+        }
     }
 }
