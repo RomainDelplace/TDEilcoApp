edb08fb4a5ddbef2b2194d3006fc11dcf9ed756a
Ophelie <baraophelie@orange.fr>
2021-12-14 18:14:28 +0100

    Update MainActivity.java

--- TP_Film/app/src/main/java/com/example/tp_film/MainActivity.java
+++ TP_Film/app/src/main/java/com/example/tp_film/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.layout.nav_bar);
+        BottomNavigationView bottomNav = findViewById(R.id.nav_bar);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
 
