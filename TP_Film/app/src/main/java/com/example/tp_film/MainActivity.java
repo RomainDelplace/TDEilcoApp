f87d6284be64e57a11a4ac3f7b8ab976b6ca0224
Ophelie <baraophelie@orange.fr>
2021-12-14 18:04:17 +0100

    Création des frames
    erreurs sur l'appel de la nav_bar. à revoir

--- TP_Film/app/src/main/java/com/example/tp_film/MainActivity.java
+++ TP_Film/app/src/main/java/com/example/tp_film/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.tp_film;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.layout.nav_bar);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item ){
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.layout.frame_popular:
+                            selectedFragment = new FramePopular();
+                            break;
+                        case R.layout.frame_search:
+                            selectedFragment = new FrameSearch();
+                            break;
+                        case R.layout.frame_favorite:
+                            selectedFragment = new FrameFavorite();
+                            break;
+                        case R.layout.frame_upcoming:
+                            selectedFragment = new FrameUpcoming();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, selectedFragment).commit();
+                    return true;
+                }
+            };
 }
